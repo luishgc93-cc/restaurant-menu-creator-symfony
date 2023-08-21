6d96abd15b9541a167da625619513f732a02c978
lhomero <lhomero@ces.vocento.com>
2023-08-21 12:05:16 +0200

    Add static orchestrator and controller

---
+++ src/Infrastructure/Controller/StaticController.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Este archivo pertenece a la aplicación XYZ.
+ * Todos los derechos reservados.
+ *
+ * @category Controlador
+ * @package  App\Infrastructure\Controller
+ * @license  Todos los derechos reservados
+ */
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class StaticController extends AbstractController
+{
+    public function homeAction(): Response
+    {
+
+        return $this->render('/Home/home.html.twig');
+
+    }
+
+}
