80c867556fe0b927bab662cc763825e222071fde
lhomero <luis.gonzales@oatobservatorio.com>
2023-10-07 12:52:31 +0200

    Modify faq static page and static controller

--- src/Infrastructure/Controller/StaticController.php
+++ src/Infrastructure/Controller/StaticController.php
@@ -13,9 +13,7 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class StaticController extends AbstractController
